9d7942541f8e5c62e9545d8f0f1843d719df94b8
Fyunka <fyunkafinka@hotmail.com>
2020-02-04 21:37:11 +0500

    init: start project graphql-nodejs-hapi-api

---
+++ README.md
@@ -0,0 +1,17 @@
+# Create a powerful API on Node.js, GraphQL, MongoDB, Hapi and Swagger
+-
+
+### Demo
+
+![](demo.png)
+
+### Setup
+
+`$ yarn install && open http://localhost:4000 && yarn run start`
+
+### Tutorial Reference
+
+[How to set-up a powerful API with Nodejs, GraphQL, MongoDB, Hapi, and Swagger, Part I](https://levelup.gitconnected.com/how-to-setup-a-powerful-api-with-nodejs-graphql-mongodb-hapi-and-swagger-e251ac189649)
+
+
+[How to set-up a powerful API with Nodejs, GraphQL, MongoDB, Hapi, and Swagger, Part II](https://levelup.gitconnected.com/how-to-set-up-a-powerful-api-with-nodejs-graphql-mongodb-hapi-and-swagger-part-ii-80266790a3ac)
---
+++ index.js
@@ -0,0 +1,111 @@
+const hapi = require('hapi');
+const mongoose = require('mongoose');
+const { graphqlHapi, graphiqlHapi } = require('apollo-server-hapi');
+const schema = require('./graphql/schema');
+const Painting = require('./models/Painting');
+
+/* swagger section */
+const Inert = require('inert');
+const Vision = require('vision');
+const HapiSwagger = require('hapi-swagger');
+const Pack = require('./package');
+
+const server = hapi.server({
+  port: 4000,
+  host: 'localhost'
+});
+
+/* db */
+mongoose.connect('mongodb://localhost:27017/modern-api');
+
+const dbName = 'modern-api';
+
+mongoose.connection.once('open', () => {
+  console.log(`Connected to database: ${dbName}`);
+});
+
+const init = async () => {
+  await server.register([
+    Inert,
+    Vision,
+    {
+      plugin: HapiSwagger,
+      options: {
+        info: {
+          title: 'Paintings API Documentation',
+          version: Pack.version
+        }
+      }
+    }
+  ]);
+
+  await server.register({
+    plugin: graphiqlHapi,
+    options: {
+      path: '/graphiql',
+      graphiqlOptions: {
+        endpointURL: '/graphql'
+      },
+      route: {
+        cors: true
+      }
+    }
+  });
+
+  await server.register({
+    plugin: graphqlHapi,
+    options: {
+      path: '/graphql',
+      graphqlOptions: {
+        schema
+      },
+      route: {
+        cors: true
+      }
+    }
+  });
+
+  server.route([
+    {
+      method: 'GET',
+      path: '/api/v1/paintings',
+      config: {
+        description: 'Get all the paintings',
+        tags: ['api', 'v1', 'painting']
+      },
+      handler: (req, reply) => {
+        return Painting.find();
+      }
+    },
+    {
+      method: 'POST',
+      path: '/api/v1/paintings',
+      config: {
+        description: 'Get a specific painting by ID.',
+        tags: ['api', 'v1', 'painting']
+      },
+      handler: (req, reply) => {
+        const { name, url, technique } = req.payload;
+        const painting = new Painting({
+          name,
+          url,
+          technique
+        });
+
+        return painting.save();
+      }
+    }
+  ]);
+
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on('unHandledRejection', err => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+});
+
+init();
